2d347ff8910a3c7d0afcd455da44250ef27ab2eb
Brett Smith <brettdansmith@gmail.com>
2023-09-15 17:25:52 -0400

    Fix mouse, can hold down now

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,6 +1,6 @@
 const DRAW_VIEW_HEIGHT = 600;
 const DRAW_VIEW_WIDTH = 600;
-
+let isMouseDown = false;
 
 function createRandomColorGrid(numRows) {
     let gridContainer = document.querySelector('.grid-container');
@@ -12,9 +12,19 @@ function createRandomColorGrid(numRows) {
         gridCell.style.outline = '1px solid grey';
         gridCell.style.display = 'inline-block';
 
-        gridCell.addEventListener('click', function() {
+        gridCell.addEventListener('mousedown', function() {
+            isMouseDown = true;
             let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
             gridCell.style.backgroundColor = randomColor;
+        });
+        gridCell.addEventListener('mouseover', () => {
+            if(isMouseDown) {
+                let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                gridCell.style.backgroundColor = randomColor;
+            }
+        });
+        gridCell.addEventListener('mouseup', () => {
+            isMouseDown = false;
         });
         gridContainer.appendChild(gridCell);
     }
