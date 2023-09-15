15dc6f78fd5d9033c0813105a6217cd542f053d1
Brett Smith <brettdansmith@gmail.com>
2023-09-15 17:20:15 -0400

    Got working with random colors on click

--- scripts/draw.js
+++ scripts/draw.js
@@ -2,18 +2,24 @@ const DRAW_VIEW_HEIGHT = 600;
 const DRAW_VIEW_WIDTH = 600;
 
 
-function createGrid(numRows) {
+function createRandomColorGrid(numRows) {
     let gridContainer = document.querySelector('.grid-container');
     gridContainer.setAttribute('style', `height: ${DRAW_VIEW_HEIGHT}px; width: ${DRAW_VIEW_WIDTH}px`);
     for (let i = 0; i < numRows * numRows; i++) {
         let gridCell = document.createElement('div');
         gridCell.style.height = `${DRAW_VIEW_HEIGHT / numRows}px`;
         gridCell.style.width = `${DRAW_VIEW_WIDTH / numRows}px`;
-        gridCell.style.background = 'pink';
         gridCell.style.outline = '1px solid grey';
         gridCell.style.display = 'inline-block';
+
+        gridCell.addEventListener('click', function() {
+            let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            gridCell.style.backgroundColor = randomColor;
+        });
         gridContainer.appendChild(gridCell);
     }
 }
 
-createGrid(3);
+
+
+createRandomColorGrid(10);
